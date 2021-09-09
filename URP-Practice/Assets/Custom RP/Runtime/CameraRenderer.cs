16f8820c108f82f617eda0430b8316206dfffdc9
April <568270833@qq.com>
2021-09-09 18:40:42 +0800

    摄像机渲染

---
+++ URP-Practice/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// 用于渲染单个摄像机
+/// </summary>
+public class CameraRenderer
+{
+    private ScriptableRenderContext context;
+    private Camera camera;
+
+    public void Render(ScriptableRenderContext context, Camera camera)
+    {
+        this.context = context;
+        this.camera = camera;
+    }
+}
---
+++ URP-Practice/Assets/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CustomRenderPipeline : RenderPipeline
+{
+    private CameraRenderer renderer = new CameraRenderer();
+
+    /// <summary>
+    /// Render方法 Unity每一帧都会调用
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="cameras"></param>
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        foreach (var camera in cameras)
+        {
+            renderer.Render(context, camera);
+        }
+    }
+}
