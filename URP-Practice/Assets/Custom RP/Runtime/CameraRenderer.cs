8d128e42999014fade7f72f0f344c28d81279ec3
April <568270833@qq.com>
2021-09-10 11:39:11 +0800

    Update CameraRenderer.cs

--- URP-Practice/Assets/Custom RP/Runtime/CameraRenderer.cs
+++ URP-Practice/Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -11,9 +11,68 @@ public class CameraRenderer
     private ScriptableRenderContext context;
     private Camera camera;
 
+    private const string bufferName = "Render Camera";
+
+    private readonly CommandBuffer buffer = new CommandBuffer {name = bufferName};
+
+    private CullingResults cullingResults;
+
     public void Render(ScriptableRenderContext context, Camera camera)
     {
         this.context = context;
         this.camera = camera;
+
+        if (!Cull())
+        {
+            return;
+        }
+        
+        Setup();
+        DrawVisibleGeometry();
+        Submit();
+    }
+
+    private void Setup()
+    {
+        context.SetupCameraProperties(camera);
+        
+        //之前已经画过得东西仍然存在，可能会干扰现在渲染的图像。为了保证正常渲染，必须清除渲染目标，消除旧的内容
+        buffer.ClearRenderTarget(true, true, Color.clear);
+        
+        buffer.BeginSample(bufferName);
+        
+        ExcuteBuffer();
+    }
+
+    /// <summary>
+    /// 向上下文发出的命令都是缓冲的，所以要做提交处理
+    /// </summary>
+    private void Submit()
+    {
+        buffer.EndSample(bufferName);
+        ExcuteBuffer();
+        context.Submit();
+    }
+
+    private void DrawVisibleGeometry()
+    {
+        context.DrawSkybox(camera);
+    }
+
+    private void ExcuteBuffer()
+    {
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Clear();
+    }
+
+    private bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+
+        return false;
     }
 }
