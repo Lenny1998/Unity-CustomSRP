254954343caa9178c1d426f609003c006344f15b
April <568270833@qq.com>
2021-09-17 18:04:50 +0800

    Unlit Shader

---
+++ URP-Practice/Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
@@ -0,0 +1,11 @@
+﻿#ifndef CUSTOM_UNLIT_INPUT_INCLUDED
+#define CUSTOM_UNLIT_INPUT_INCLUDED
+
+float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;
+float4x4 unity_MatrixVP;
+float4x4 unity_MatrixV;
+float4x4 glstate_matrix_projection;
+real4 unity_WorldTransformParams;
+
+#endif
