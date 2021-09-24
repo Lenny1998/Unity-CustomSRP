d385829e246d15a8f6d6a7c4cc9298907e8869da
April <568270833@qq.com>
2021-09-24 15:23:49 +0800

    多实例 gpu instancing测试

---
+++ URP-Practice/Assets/Custom RP/Examples/MeshBall.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MeshBall : MonoBehaviour
+{
+    private static int baseColorId = Shader.PropertyToID("_BaseColor");
+
+    [SerializeField] private Mesh mesh = default;
+
+    [SerializeField] private Material material = default;
+
+    private Matrix4x4[] matrices = new Matrix4x4[1023];
+    private Vector4[] baseColors = new Vector4[1023];
+
+    private MaterialPropertyBlock block;
+
+    private void Awake()
+    {
+        for (int i = 0; i < matrices.Length; i++)
+        {
+            matrices[i] = Matrix4x4.TRS(Random.insideUnitSphere * 10f, Quaternion.identity, Vector3.one);
+            baseColors[i] = new Vector4(Random.value, Random.value, Random.value, 1f);
+        }
+    }
+
+    private void Update()
+    {
+        if (block == null)
+        {
+            block = new MaterialPropertyBlock();
+            block.SetVectorArray(baseColorId, baseColors);
+        }
+
+        Graphics.DrawMeshInstanced(mesh, 0, material, matrices, 1023, block);
+    }
+}
